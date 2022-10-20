c25cab474fc4040cabee1cd3c445a360719fbb2d
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2022-10-20 14:24:12 -0300

    Make medal browsing clientside

--- osekaiplus.user.js
+++ osekaiplus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        osekaiplus
 // @namespace   https://pedro.moe
-// @version     1.7.3
+// @version     1.8.0
 // @description Improve user experience on osekai.net (osu! medals website)
 // @author      EXtemeExploit
 // @match       http://osekai.net/*
@@ -16,18 +16,28 @@
 
 	var MEDALS_RARITY_URL = 'https://osekai.net/rankings/api/upload/scripts-rust/down_rarity.php';
 	var MedalsRarityArray = []; // This one is sorted by rarity
+	var MedalsAchievedFilterArray = [];
 
 	document.addEventListener('DOMContentLoaded', () => {
-		reloadosekaiPlus();
 		// Function to override to fix them or something
 		if (typeof LoadRecentlyViewed !== 'undefined') LoadRecentlyViewed = LoadRecentlyViewedPatched;
-	});
+		if (typeof filterAchieved !== 'undefined') filterAchieved = filterAchievedPatched;
+		if (typeof requestMedals !== 'undefined') requestMedals = requestMedalsPatched;
+
+		reloadosekaiPlus();
+	}, { capture: true });
+
 
 	function reloadosekaiPlus() {
-		setInterval(giveMedalsRarityRankingCountLoader, 250);
-		setInterval(giveColorsToUsersInRankingsLoader, 250);
-		setInterval(profilesPatchesLoader, 250);
-		setInterval(giveMedalsRarityCountLoader, 250);
+
+		setInterval(loadIntervalPatches, 250);
+		function loadIntervalPatches() {
+			giveMedalsRarityRankingCountLoader();
+			giveColorsToUsersInRankingsLoader();
+			profilesPatchesLoader();
+			giveMedalsRarityCountLoader();
+			betterMedalsFilteringLoader();
+		}
 
 		simplifyRescrictionBanner(); // This doesn't need an interval
 
@@ -47,22 +57,6 @@
 			}
 		}
 
-		async function getMedalsCount() {
-			return new Promise((resolve) => {
-				let xhr = new XMLHttpRequest();
-				xhr.open('GET', MEDALS_RARITY_URL, true);
-				xhr.onload = () => {
-					if (xhr.status === 200) {
-						let oResponse = JSON.parse(xhr.responseText);
-						resolve(oResponse.sort((a, b) => {
-							return a.count - b.count;
-						}));
-					}
-				};
-				xhr.send();
-			});
-		}
-
 		/**
 		 * 
 		 * @returns Current page minus 1, so first page is 0
@@ -75,7 +69,7 @@
 		async function giveMedalsRarityRankingCountLoader() {
 			if (document.URL.startsWith('https://osekai.net/rankings/?ranking=Medals&type=Rarity')) {
 				// Want to get medals rarity count first, await it so no race condition
-				if (MedalsRarityArray.length == 0)// Only make the request if needed
+				if (MedalsRarityArray.length == 0) // Only make the request if needed
 					MedalsRarityArray = await getMedalsCount();
 				giveMedalsRarityRankingCount();
 			}
@@ -162,45 +156,205 @@
 				document.getElementById('strMedalRarity').innerHTML += ` (${result[0].count} #${rarityRanking})`;
 			}
 		}
+
+
+
+		function txtMedalSearchEventListener() {
+			filterAchieved(document.getElementById('styled-checkbox-1').checked);
+		}
+
+		async function betterMedalsFilteringLoader() {
+			if (document.URL.startsWith('https://osekai.net/medals/')) {
+				document.getElementById('txtMedalSearch').removeEventListener('input', txtMedalSearchEventListener);
+				document.getElementById('txtMedalSearch').addEventListener('input', txtMedalSearchEventListener);
+			}
+		}
+
+
+	} // Ready ends here
+
+
+	async function getMedalsCount() {
+		return new Promise((resolve) => {
+			let xhr = new XMLHttpRequest();
+			xhr.open('GET', MEDALS_RARITY_URL, true);
+			xhr.onload = () => {
+				if (xhr.status === 200) {
+					let oResponse = JSON.parse(xhr.responseText);
+					resolve(oResponse.sort((a, b) => {
+						return a.count - b.count;
+					}));
+				}
+			};
+			xhr.send();
+		});
 	}
-})();
-
-function LoadRecentlyViewedPatched() {
-	let mostPopular = false;
-	// if the user is logged out, set mostPopular to true
-	if (nUserID == -1) {
-		mostPopular = true;
+	async function getMedalsFilterArray() {
+		return new Promise((resolve) => {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', 'https://osekai.net/medals/api/filters.php', true);
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+			xhr.onload = () => {
+				if (xhr.status === 200) {
+					let oResponse = JSON.parse(xhr.responseText);
+					resolve(oResponse);
+				}
+			};
+			xhr.send('strSearch=');
+		});
 	}
 
-	let xhr = new XMLHttpRequest();
 
-	let url = '';
-	if (mostPopular)
-		url = 'https://osekai.net/profiles/api/most_visited';
-	else
-		url = 'https://osekai.net/profiles/api/recent_visits';
-	xhr.open('GET', url, true);
-	xhr.onload = function () {
-		if (this.status == 200) {
-			let json = JSON.parse(this.responseText);
-			let html = '';
+	// Patched functions go here, outside the ready scope
 
-			for (let i = 0; i < json.length; i++) {
-				if (json[i].userdata == null) continue;
-				html += `
+	function LoadRecentlyViewedPatched() {
+		let mostPopular = false;
+		// if the user is logged out, set mostPopular to true
+		if (nUserID == -1) {
+			mostPopular = true;
+		}
+
+		let xhr = new XMLHttpRequest();
+
+		let url = '';
+		if (mostPopular)
+			url = 'https://osekai.net/profiles/api/most_visited';
+		else
+			url = 'https://osekai.net/profiles/api/recent_visits';
+		xhr.open('GET', url, true);
+		xhr.onload = function () {
+			if (this.status == 200) {
+				let json = JSON.parse(this.responseText);
+				let html = '';
+
+				for (let i = 0; i < json.length; i++) {
+					if (json[i].userdata == null) continue;
+					html += `
 				<div class="profiles__ranking-user" onclick="loadUser(${json[i].visited_id});">
 					<img src="https://a.ppy.sh/${json[i].visited_id}" class="profiles__ranking-pfp">
 					<div class="profiles__ranking-texts">
 						<p class="profiles__ranking-username">${json[i].userdata.Username}</p>
 					</div>
 				</div>`;
+				}
+
+				document.getElementById('recentlyviewed').innerHTML = html;
+			} else {
+				console.log('error');
+			}
+		};
+
+		xhr.send();
+	}
+
+	async function filterAchievedPatched(on) {
+		if (MedalsAchievedFilterArray.length == 0) {
+			MedalsAchievedFilterArray = await getMedalsFilterArray();
+		}
+
+		if (on) {
+			for (let i = 0; i < MedalsAchievedFilterArray.length; i++) {
+				let medalid = MedalsAchievedFilterArray[i];
+				if (document.getElementById('medal_' + medalid)) {
+					document.getElementById('medal_' + medalid).classList.add('medals__medal-filtered');
+				}
+			}
+		} else {
+			let filteredMedals = document.getElementsByClassName('medals__medal-filtered').length;
+			for (let i = 0; i < filteredMedals; i++) {
+				document.getElementsByClassName('medals__medal-filtered')[0].classList.remove('medals__medal-filtered');
 			}
 
-			document.getElementById('recentlyviewed').innerHTML = html;
-		} else {
-			console.log('error');
 		}
-	};
+	}
 
-	xhr.send();
-}
+	// I can't just change the event listener of the input field, so i have to override the entire thing :skull:
+	// This is super scuffed, but its the best i could do
+	async function requestMedalsPatched(bInit, strKey, strValue, strUrl) {
+		if (bInit) {
+			var xhr = createXHR(strUrl);
+			xhr.send(strKey + '=' + strValue);
+
+			xhr.onreadystatechange = function () {
+				document.getElementById('oMedalSection').innerHTML = '';
+				var oResponse = getResponse(xhr);
+				let strLastRestriction;
+				if (handleUndefined(oResponse)) return;
+				Object.keys(oResponse).forEach(function (obj) {
+					let imgList = '';
+					if (handleUndefined(obj)) return;
+					oResponse[obj].forEach(function (innerObj) {
+						// console.log(innerObj.Name)
+						colMedals[innerObj.Name] = innerObj;
+						if (innerObj.Name == 'chromb') return;
+						if ((typeof strLastRestriction !== 'undefined' && strLastRestriction !== null) && innerObj.Restriction !== strLastRestriction) imgList += '</div><div class="osekai__divider"></div><div class="medals__grid">';
+						imgList += '<div class="medals__grid-medal-container" data-tippy-content="' + innerObj.Name + '">';
+						// if date is not null, and it is less than a week ago, it's new!
+						// console.log(innerObj);
+						if (innerObj.Date != null) {
+							let date = new Date(innerObj.Date);
+							let now = new Date();
+							let diff = now.getTime() - date.getTime();
+							// console.log(diff);
+							if (diff < 604800000) {
+								// console.log("new");
+								imgList += '<div class="new-badge">NEW</div>';
+							}
+						}
+						imgList += '<img onload="medalImageLoaded(this)" onclick="changeState(\'' + innerObj.Name.replace(/'/g, '\\\'') + '\')" alt="' + innerObj.Name + '" id="medal_' + innerObj.MedalID + '"  class="medals__grid-medal lazy" src="' + innerObj.Link + '">';
+						imgList += '</div>';
+						strLastRestriction = innerObj.Restriction;
+					});
+					strLastRestriction = null;
+					oMedalSection.innerHTML += '<section class="osekai__panel"><div class="osekai__panel-header"><p>' + obj + '</p></div><div class="osekai__panel-inner"><div class="medals__grid-container"><div class="medals__grid">' + imgList + '</div></div></div></section>';
+				});
+				tippy('[data-tippy-content]', {
+					appendTo: document.getElementsByClassName('medals__scroller')[0],
+					arrow: true,
+				});
+
+				if (new URLSearchParams(window.location.search).get('medal') !== null) loadMedal(new URLSearchParams(window.location.search).get('medal'));
+			};
+		} else {
+			// Patched part here :)
+			document.getElementById('oMedalSection').innerHTML = '';
+
+			let colMedalsArray = Object.values(colMedals);
+
+			let filteredMedalsArray = colMedalsArray.filter((e) => {
+				return e.Name.toLowerCase().includes(strValue.toLowerCase());
+			});
+
+			let arr = [];
+			for (const [k, v] of Object.entries(filteredMedalsArray)) {
+				if (arr[v.Grouping] == null) arr[v.Grouping] = [];
+				arr[v.Grouping].push(v);
+			}
+			let strLastRestriction;
+			Object.keys(arr).forEach((obj) => {
+				let imgList = '';
+				arr[obj].forEach((innerObj) => {
+					// console.log(innerObj.Name)
+					arr[innerObj.Name] = innerObj;
+					if (innerObj.Name == 'chromb') return;
+					if ((typeof strLastRestriction !== 'undefined' && strLastRestriction !== null) && innerObj.Restriction !== strLastRestriction) imgList += '</div><div class="osekai__divider"></div><div class="medals__grid">';
+					imgList += '<div class="medals__grid-medal-container" data-tippy-content="' + innerObj.Name + '">';
+					if (innerObj.Date != null) { // It has a date!, check if its less than a week old
+						let date = new Date(innerObj.Date);
+						let now = new Date();
+						let diff = now.getTime() - date.getTime();
+						if (diff < 604800000) {
+							// IT IS!, add the new badge 8)
+							imgList += '<div class="new-badge">NEW</div>';
+						}
+					}
+					imgList += '<img onload="medalImageLoaded(this)" onclick="changeState(\'' + innerObj.Name.replace(/'/g, '\\\'') + '\')" alt="' + innerObj.Name + '" id="medal_' + innerObj.MedalID + '"  class="medals__grid-medal lazy" src="' + innerObj.Link + '">';
+					imgList += '</div>';
+					strLastRestriction = innerObj.Restriction;
+				});
+				strLastRestriction = null;
+				oMedalSection.innerHTML += '<section class="osekai__panel"><div class="osekai__panel-header"><p>' + obj + '</p></div><div class="osekai__panel-inner"><div class="medals__grid-container"><div class="medals__grid">' + imgList + '</div></div></div></section>';
+			});
+		}
+	}
+})();
